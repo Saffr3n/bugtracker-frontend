b1e6bc31d51796c2918f98a8f44a35422cf3a6aa
Saffr3n <saffr3n@gmail.com>
2023-02-23 14:41:18 +0200

    Create and style Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import ghIcon from '../../assets/icons/github.svg';
 import './footer.scss';
 
 export default function Footer() {
-  return <footer>FOOTER</footer>;
+  return (
+    <footer>
+      <small>
+        Copyright &copy; 2023{' '}
+        <a href="https://github.com/saffr3n" target="_blank" rel="noreferrer">
+          <img src={ghIcon} alt="Open authors GitHub" />
+          Saffr3n
+        </a>
+      </small>
+    </footer>
+  );
 }
