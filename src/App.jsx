f0315eac9dd69a3dbd2b5466d75711a227a7abb2
Saffr3n <saffr3n@gmail.com>
2023-02-24 09:41:20 +0200

    Normalize and edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
-import './assets/style.scss';
+import './assets/styles/style.scss';
 
 export default function App() {
   return (
